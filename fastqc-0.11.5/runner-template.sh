5623b8f2f4ad993711126ffb365c07515e10cb8d
alejandrox1 <alarcj137@gmail.com>
2018-07-09 09:57:39 -0500

    fastqc-0.11.5/ moved inside fastqc/
    Signed-off-by: alejandrox1 <alarcj137@gmail.com>

--- fastqc-0.11.5/runner-template.sh
+++
@@ -1,9 +0,0 @@
-CONTAINER_IMAGE="index.docker.io/alejandrox1/fastqc:0.11.5"
-
-. _util/container_exec.sh
-
-
-COMMAND="fastqc"
-PARAMS="${fastqc}" # $fastqc will be defined later by agave
-
-container_exec ${CONTAINER_IMAGE} ${COMMAND} ${PARAMS}
